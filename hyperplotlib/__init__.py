b00099646b74929441db668ea6529afc31241978
SimonBlanke <simonblanke@hotmail.de>
2020-04-02 11:10:34 +0200

    remove unused files/code

--- hyperplotlib/__init__.py
+++ hyperplotlib/__init__.py
@@ -4,9 +4,3 @@
 
 __version__ = "0.0.1"
 __license__ = "MIT"
-
-
-from .hyperplotlib import Hyperplotlib
-
-
-__all__ = ["Hyperplotlib"]
--- hyperplotlib/hyperplotlib_api.py
+++
@@ -1,9 +0,0 @@
-# Author: Simon Blanke
-# Email: simon.blanke@yahoo.com
-# License: MIT License
-
-
-class Hyperplotlib:
-    def __init__(self):
-	pass
-
