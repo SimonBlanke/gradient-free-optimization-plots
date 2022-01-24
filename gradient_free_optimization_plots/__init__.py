3acca2a50817047216381ec15d0374dcee69cf4b
Simon Blanke <simon.blanke@yahoo.com>
2022-01-24 20:03:49 +0100

    add search_path_gif-script

--- gradient_free_optimization_plots/__init__.py
+++ gradient_free_optimization_plots/__init__.py
@@ -6,10 +6,9 @@ __version__ = "0.0.1"
 __license__ = "MIT"
 
 
-from .plot_objective_function import plot_objective_function_2d
+from .search_path_gif import search_path_gif
 
 
 __all__ = [
-    "plot_objective_function_2d",
+    "search_path_gif",
 ]
-
